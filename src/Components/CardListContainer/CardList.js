4fa2ff15e8975aef3ffdf3070bf6427601d099e9
Marcos-calisse <marcos.andres611@gmail.com>
2022-05-26 20:31:53 -0300

    Clase 5: Tome productos del backend y los mapie

--- src/Components/CardListContainer/CardList.js
+++ src/Components/CardListContainer/CardList.js
@@ -1,18 +1,69 @@
 import CardItem from '../Cards/Card'
 import Container from 'react-bootstrap/Container'
 import './cardList.css'
+import { useEffect, useState } from 'react'
 
 const CardList = ({titulo}) => {
+    
+    const [products, setProductos] = useState([])
+
+    const productos = [
+        {
+            id: 1,
+            nombre: "Boxer Armour",
+            precio: 1500,
+            stock: 10,
+            image: './boxer-Armound.jpg'
+        },
+        {
+            id: 2,
+            nombre: "Gorras",
+            precio: 2299,
+            stock: 20,
+            image: './gorras.jpg'
+        },
+        {
+            id: 3,
+            nombre: "Boxer Tommy",
+            precio: 2000,
+            stock: 15,
+            image: './boxer-tommy.jpg'
+        }
+    ]
+
+    const getProducts = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() =>{
+                resolve(productos)
+            }, 2000)
+        })
+    }
+
+    useEffect(() => {
+        getProducts()
+        .then((response) =>{
+            setProductos(response)
+        })
+        .catch((error) =>{
+            console.log("error de carga de datos", error)
+        })
+    },[])
+   
+
     return(
         <>
             <h2>{titulo}</h2>
             <Container className="containerCards">
-                <CardItem titulo='Boxer Armour' precio={1500} image={'boxer-Armound.jpg'} />
-
-                <CardItem titulo='Gorras' precio={2299} image={'gorras.jpg'} />
-
-                <CardItem titulo='Boxer Tommy' precio={2000} image={'boxer-tommy.jpg'} />
-
+                {
+                    products.map((producto) => {
+                        console.log('id actual:', producto)
+                        return(
+                            <div>
+                                <CardItem titulo={producto.nombre} precio={producto.precio} image={producto.image} /> 
+                            </div>
+                        )
+                    })
+                }
             </Container>
         </>
     )
