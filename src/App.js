a1d6ed8c66aa97b315372793c6708f6e98f199d6
Marcos-calisse <marcos.andres611@gmail.com>
2022-05-30 22:16:08 -0300

    Agregue la dependencia de react-route-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import NavBar from './Components/Nav/NavBar';
 //import CardList from './Components/CardListContainer/CardList';
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
       <div className="App">
-        <NavBar />
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route />
+          </Routes>
+        </BrowserRouter>
+        
         <h1>Armen - Ropa interior masculina</h1>
         {/* <CardList titulo={"PRODUCTOS MAS BUSCADOS"}/> */}
         <ItemDetailContainer />
