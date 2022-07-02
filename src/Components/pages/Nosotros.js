969c5eb0308926a1a8a72371bf346a96fb329523
Marcos-calisse <marcos.andres611@gmail.com>
2022-07-02 18:13:34 -0300

    entrega final con archivo readme modificado

--- src/Components/pages/Nosotros.js
+++ src/Components/pages/Nosotros.js
@@ -10,7 +10,9 @@ const Nosotros = () => {
                     Somos un proyecto que siempre mira al futuro, buscando satisfacer la necesidad de cada persona. Ofrecemos productos y accesorios para hombres de distintas lineas, donde los compradores podrán obtenerlo desde la comodidad de su hogar. 
                     Ar-Men busca siempre tener la mayor variedad de mercancía así como de productos temporales o de edición limitada que se adaptan a cualquier presupuesto.
                 </p>
-                <img src='./imgNosotros.webp' alt='' />
+                <div className="containerImgNosotros">
+                    <img src='./imgNosotros.webp' alt='' />
+                </div>
             </Container>
         </div>
     )
